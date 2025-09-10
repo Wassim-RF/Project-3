bc05d8a7300c372e4eb47407160ef763a2f5b85d
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-10 11:32:57 +0200

    project3

---
+++ project3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_U 100
+
+int main() {
+    char utilisateur[MAX_U];
+
+    while (1) {
+        printf("UTILISATEUR : ");
+        fgets(utilisateur , MAX_U , stdin);
+        utilisateur[strcspn(utilisateur , "\n")] = '\0';
+        if (strcmp(utilisateur, "exit") == 0) {
+            printf("ChatBot : Au revoir\n");
+            break;
+        }
+    }
+}
